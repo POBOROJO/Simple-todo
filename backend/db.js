e8aa2d2995b69d8d044fa2e6818ce163206a4085
Parijat Bhattacharjee <94280580+POBOROJO@users.noreply.github.com>
2024-01-13 12:31:32 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -8,4 +8,16 @@
 
 const mongoose = require("mongoose");
 
-mongoose.connect("")
+mongoose.connect("");
+
+const todoSchema = mongoose.Schema({
+    title: String,
+    description : String,
+    completed : Boolean
+})
+
+const todo = mongoose.model('todos',todoSchema);
+
+module.exports = {
+    todo
+}
